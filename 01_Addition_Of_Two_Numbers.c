dcc1fc929c21d89e35ffe8e07a0a20d3c464646d
Shrey Kulkarni <shrey280302@gmail.com>
2025-06-25 10:30:41 +0530

    Add first file of the course and also added .gitignore file, with ignoring .exe files.

---
+++ 01_Addition_Of_Two_Numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int a = 5;
+    int b = 10;
+    int sum = a + b;
+    printf("The sum of %d and %d is: %d\n", a, b, sum);
+    // The output will be: The sum of 5 and 10 is: 15
+    
+    return 0;
+}
