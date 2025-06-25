e5285cbe22e2abef8f79f87aaaabebee549971ed
Shrey Kulkarni <shrey280302@gmail.com>
2025-06-25 18:31:45 +0530

    Add 7th file to calculate perimeter of rectangle.

---
+++ Assignment_0/07_Perimeter_of_rectangle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int length, breadth;
+
+    printf("Enter the length and breadth of the rectangle:\n");
+    scanf("%d", &length);
+    scanf("%d", &breadth);
+
+    int perimeter = 2 * (length + breadth);
+
+    printf("The perimeter of the rectangls is : %d", perimeter);
+    return 0;
+}
