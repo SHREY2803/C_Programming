ceaff4aa8d6d00d7a40ee630346ccfc961b8f424
Shrey Kulkarni <shrey280302@gmail.com>
2025-07-02 22:34:36 +0530

    Add file for getting sum from range of numbers.

---
+++ Assignment_3/03_Sum_of_Numbers_in_given_range.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2;
+
+    printf("Enter the numbers n1 and n2, to find the sum of numbers from the range of n1 and n2: \n");
+    scanf("%d", &n1);
+    scanf("%d", &n2);
+
+    int sum = 0;
+
+    if (n1 < n2)
+    {
+        while (n1 <= n2)
+        {
+            sum = sum + n1;
+            n1++;
+        }
+    }
+    else
+    {
+        while (n2 <= n1)
+        {
+            sum = sum + n2;
+            n2++;
+        }
+    }
+
+    printf("The total sum of numbers from range of n1 and n2 is %d\n", sum);
+    return 0;
+}
