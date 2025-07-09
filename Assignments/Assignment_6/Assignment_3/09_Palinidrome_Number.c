7d8991d5c298d72b63136777c863e4cee0895a5a
Shrey Kulkarni <shrey280302@gmail.com>
2025-07-09 10:13:02 +0530

    x

---
+++ Assignments/Assignment_6/Assignment_3/09_Palinidrome_Number.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter your number here: \n");
+    scanf("%d", &n);
+
+    if (n < 0)
+    {
+        printf("negative number is not a palindrome.");
+        return 0;
+    }
+
+    int temp = n;
+    int rev = 0;
+
+    while (temp)
+    {
+        int ld = temp % 10;
+        rev = (rev * 10) + ld;
+        temp = temp / 10;
+    }
+
+    if (rev == n)
+    {
+        printf("Palindrome Number\n");
+    }
+    else
+    {
+        printf("Not a palindrome Number\n");
+    }
+    return 0;
+}
