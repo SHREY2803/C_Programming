4c6cc02a141409203a9fb4f6365c7d6cc3883088
Shrey Kulkarni <shrey280302@gmail.com>
2025-07-14 14:39:39 +0530

    Add type2 function logic to wach question.

---
+++ Assignments/Assignment_6/Type-2 (Function)/Assignment_4/01_Armstrong_numbers_given_range.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+
+int calDigits(int n)
+{
+    int digits = 0;
+    while (n)
+    {
+        digits++;
+        n = n / 10;
+    }
+    return digits;
+}
+
+void checkArmstrongRange(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        int arm = 0;
+        int temp = i;
+        int digits = calDigits(temp);
+
+        while (temp)
+        {
+            int ld = temp % 10;
+            arm += pow(ld, digits);
+            temp = temp / 10;
+        }
+
+        if (arm == i)
+        {
+            printf("%d is a armstrong number\n", i);
+        }
+    }
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter your number here: \n");
+    scanf("%d", &n);
+
+    checkArmstrongRange(n);
+    return 0;
+}
