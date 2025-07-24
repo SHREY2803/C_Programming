b08cb24e69083891ae46d036990946ba3aa30505
Shrey Kulkarni <shrey280302@gmail.com>
2025-07-24 16:58:41 +0530

    Minor changes in the logic of the answer.

--- Assignments/Assignment_6/Type-3 (Function)/Assignment_2/05_Discount_Calculator.c
+++ Assignments/Assignment_6/Type-3 (Function)/Assignment_2/05_Discount_Calculator.c
@@ -1,47 +1,59 @@
 #include <stdio.h>
 
-int discountCalculator()
+// Global variables
+char check_student;
+double total_price;
+
+double discountCalculator()
 {
-    char check_student;
-    double total_price;
-
-    printf("Enter your total purchased price: \n");
-    scanf("%lf", &total_price);
-
-    printf("Are you a student? (y/n): ");
-    scanf(" %c", &check_student);
-
     if (check_student == 'y')
     {
         if (total_price >= 500)
         {
-            total_price = total_price - (0.2 * total_price);
-            // printf("Your total price after '20%' discount is : %0.2lf ", total_price);
+            return total_price - (0.2 * total_price);
         }
         else
         {
-            total_price = total_price - (0.1 * total_price);
-            // printf("Your total price after '10%' discount is : %0.2lf", total_price);
+            return total_price - (0.1 * total_price);
         }
     }
     else
     {
         if (total_price >= 600)
         {
-            total_price = total_price - (0.15 * total_price);
-            // printf("Your total price after '15%' discount is : %0.2lf", total_price);
+            return total_price - (0.15 * total_price);
         }
         else
         {
-            // printf("No discount final price is : %0.2lf", total_price);
+            return total_price; // no discount
         }
     }
 }
 
 int main()
 {
+    printf("Enter your total purchased price: \n");
+    scanf("%lf", &total_price);
 
-    discountCalculator();
+    printf("Are you a student? (y/n): ");
+    scanf(" %c", &check_student); // leading space avoids newline issues
+
+    double result = discountCalculator();
+
+    if (check_student == 'y')
+    {
+        if (total_price >= 500)
+            printf("Your total price after '20%%' discount is: %.2lf\n", result);
+        else
+            printf("Your total price after '10%%' discount is: %.2lf\n", result);
+    }
+    else
+    {
+        if (total_price >= 600)
+            printf("Your total price after '15%%' discount is: %.2lf\n", result);
+        else
+            printf("No discount. Final price is: %.2lf\n", result);
+    }
 
     return 0;
 }
